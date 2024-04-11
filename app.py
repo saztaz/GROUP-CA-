ba5b0dfbc48358415448cedc3af7a49610a41f92
Sazzad <100172469+saztaz@users.noreply.github.com>
2024-04-11 12:37:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root$@123'
+app.config['MYSQL_DB'] = 'covid'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
