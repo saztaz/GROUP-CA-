a0d33806957f5093041733a9632beebf54520bb5
Sazzad <100172469+saztaz@users.noreply.github.com>
2024-04-11 12:37:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,20 @@ mysql = MySQL(app)
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        mysql.connection.commit()
+        cur.close()
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
