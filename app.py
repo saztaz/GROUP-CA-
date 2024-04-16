cb5c3b472e90e703cab598db78f77d75d26be009
Sazzad <100172469+saztaz@users.noreply.github.com>
2024-04-16 11:20:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from passlib.hash import sha256_crypt
 from functools import wraps
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root$@123'
 
 
 
