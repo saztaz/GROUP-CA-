e9caa54cf45e36440e8485cb9d43880e8d125466
Sazzad <100172469+saztaz@users.noreply.github.com>
2024-04-16 11:24:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root$@123'
+app.config['MYSQL_DB'] = 'covid'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
 
 
 
