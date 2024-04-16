e3be9fec57d6ac28cd8035b058f1c1831d475216
Sazzad <100172469+saztaz@users.noreply.github.com>
2024-04-16 20:37:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,8 @@ def contact_tracing(id):
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
     
 
 
