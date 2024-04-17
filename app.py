559c6a744206b1bb95423956fe6535d213b257d3
Ranjithkumar-Mathiazhagan <165322495+Ranjithkumar-Mathiazhagan@users.noreply.github.com>
2024-04-17 17:12:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,16 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username= request.form['username']
+        password_candidate= request.form['password']
+        
+        cur = mysql.connection.cursor()
+
+        result= cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        
+
+    
         
        
 
