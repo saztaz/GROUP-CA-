ea540d46b5fd480887e4691ea09e9cd9555cf9e0
Ranjithkumar-Mathiazhagan <165322495+Ranjithkumar-Mathiazhagan@users.noreply.github.com>
2024-04-18 11:23:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,9 @@ if result> 0:
     if sha256_crypt.verify(password_candidate, password):
         session['logged_in'] = True
         session['username'] = username
+        
+        flash('You are now logged in', 'success')
+        return redirect(url_for('dashboard'))
 
 
 
